b6bfe712c1dba9eda063160937da320e9b8501a5
Zhao Chenhui <chenhuiz@cubbli18-gold.local>
2019-11-18 20:29:26 +0200

    added .gitignore

--- src/main/java/projekti/Comment.java
+++ src/main/java/projekti/Comment.java
@@ -3,6 +3,7 @@ package projekti;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
