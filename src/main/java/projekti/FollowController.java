f2e0ee109cc6a8fce3abd83b3622a608f3fb2852
Zhao Chenhui <chenhuiz@cubbli18-gold.local>
2019-11-21 02:53:09 +0200

    Followingship done

--- src/main/java/projekti/FollowController.java
+++ src/main/java/projekti/FollowController.java
@@ -5,43 +5,33 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FollowController {
     @Autowired
     private UserRepository userRepository;
     
-    @PostMapping(value="/{userId}/search/{personId}")
-//    @Transactional
+    @PostMapping("/{userId}/follow/{personId}")
     public String follow(Model model, @PathVariable Long userId, @PathVariable Long personId) {
+        // User is going to follow person
         User user = this.userRepository.getOne(userId); 
-        User follower = this.userRepository.getOne(personId);
+        User person = this.userRepository.getOne(personId);
         
         user.setDate(LocalDate.now());
         user.setTime(LocalTime.now());
-        follower.setDate(LocalDate.now());
-        follower.setTime(LocalTime.now());
+        person.setDate(LocalDate.now());
+        person.setTime(LocalTime.now());
         
-        user.getFollowers().add(follower);
-        follower.getFollowings().add(user);
-        
+        user.setFollower(person);
         this.userRepository.save(user);
-        this.userRepository.save(follower);
         
-        return "redirect: /{userId}/search";
+        person.setFollowing(user);
+        this.userRepository.save(person);
+        
+        return "redirect:/" + userId;
     }
     
-//    @GetMapping("/{userId}/search/{id}") 
-//    public String goToFollowingPage() {
-//        User user = this.userRepository.getOne(userId); 
-//        User following = this.userRepository.getOne(id);
-//        return "";
-//    }
 }
