b8c0d8a1cb4da0093f73d86a9130d7174aa3c208
Zhao Chenhui <chenhuiz@cubbli18-gold.local>
2019-11-25 18:40:36 +0200

    code styling

--- src/main/java/projekti/SearchController.java
+++ src/main/java/projekti/SearchController.java
@@ -21,62 +21,38 @@ public class SearchController {
     // go to search page
     @GetMapping("/{userId}/search")
     public String searchPage(Model model, @PathVariable Long userId) {
+
         User user = this.userRepository.getOne(userId);
         model.addAttribute("user", user);
+        
         return "search";
     }
 
     @PostMapping("/{userId}/search")
     public String getFindings(Model model, @RequestParam String firstname, @RequestParam String familyname, @PathVariable Long userId) {
+
         User user = this.userRepository.getOne(userId);
-        model.addAttribute("user", user);
-        List<Followingship> followed = new ArrayList<>();
+        List<Followingship> followed = user.getFollowings();
+        List<User> users = new ArrayList<>();
 
         if (firstname.isEmpty() && !familyname.isEmpty()) {
-
-            List<User> familynames = this.userRepository.findByFamilyname(familyname);
-            followed = this.followingshipRepository.findByFamilyname(familyname);
-
-            for (int i = 0; i < followed.size(); i++) {
-                User followingPerson = this.userRepository.getOne(followed.get(i).getFollowing());
-                if (familynames.contains(followingPerson)) {
-                    familynames.remove(followingPerson);
-                }
-            }
-
-            model.addAttribute("findings", familynames);
-            model.addAttribute("followed", followed);
-
+            users = this.userRepository.findByFamilyname(familyname);
         } else if (familyname.isEmpty() && !firstname.isEmpty()) {
-
-            List<User> firstnames = this.userRepository.findByFirstname(firstname);
-            followed = this.followingshipRepository.findByFirstname(firstname);
-
-            for (int i = 0; i < followed.size(); i++) {
-                User followingPerson = this.userRepository.getOne(followed.get(i).getFollowing());
-                if (firstnames.contains(followingPerson)) {
-                    firstnames.remove(followingPerson);
-                }
-            }
-
-            model.addAttribute("findings", firstnames);
-            model.addAttribute("followed", followed);
-
+            users = this.userRepository.findByFirstname(firstname);
         } else if (!familyname.isEmpty() && !firstname.isEmpty()) {
-
-            List<User> users = this.userRepository.findByFirstnameAndFamilyname(firstname, familyname);
-            followed = this.followingshipRepository.findByFamilynameAndFirstname(familyname, firstname);
-
-            for (int i = 0; i < followed.size(); i++) {
-                User followingPerson = this.userRepository.getOne(followed.get(i).getFollowing());
-                if (users.contains(followingPerson)) {
-                    users.remove(followingPerson);
-                }
-            }
-
-            model.addAttribute("findings", users);
-            model.addAttribute("followed", followed);
+            users = this.userRepository.findByFirstnameAndFamilyname(firstname, familyname);
         }
+
+        for (int i = 0; i < followed.size(); i++) {
+            User followingPerson = this.userRepository.getOne(followed.get(i).getFollowing());
+            if (users.contains(followingPerson)) {
+                users.remove(followingPerson);
+            }
+        }
+
+        model.addAttribute("user", user);
+        model.addAttribute("findings", users);
+        model.addAttribute("followed", followed);
 
         return "search";
     }
