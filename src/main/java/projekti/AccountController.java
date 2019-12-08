4022a413f25abb7f8a4018cf6db217d013a46566
Zhao Chenhui <chenhuiz@cubbli18-gold.local>
2019-12-08 22:42:26 +0200

    edited pom

--- src/main/java/projekti/AccountController.java
+++ src/main/java/projekti/AccountController.java
@@ -1,11 +1,9 @@
 package projekti;
 
 import javax.transaction.Transactional;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import projekti.service.AccountService;
 import projekti.service.ThumbService;
