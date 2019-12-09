980df42f2e1d778f335a55c4eb9aef8bace46505
Zhao Chenhui <chenhuiz@cubbli18-gold.local>
2019-12-09 21:42:00 +0200

    bootstrap

--- src/main/java/projekti/FollowingMessage.java
+++ src/main/java/projekti/FollowingMessage.java
@@ -2,16 +2,8 @@
 package projekti;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
