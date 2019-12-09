b3d6784701e416ba070e166b384c92a7b7600d4b
Zhao Chenhui <chenhuiz@cubbli18-gold.local>
2019-12-10 00:37:40 +0200

    fixes

--- src/main/java/projekti/Picture.java
+++ src/main/java/projekti/Picture.java
@@ -1,17 +1,12 @@
 
 package projekti;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
